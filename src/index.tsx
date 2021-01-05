43241d9180353f821132846c940a466333a8d0b9
Amo Timothy Kofi <timdereaper1@gmail.com>
2021-01-05 10:08:41 +0000

    removed previous config

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-	<React.StrictMode>
-		<div>Hello world! working</div>
-	</React.StrictMode>,
-	document.getElementById('root')
-);
